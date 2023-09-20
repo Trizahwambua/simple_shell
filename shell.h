f746acecde1685d9493fb2d2a4a06d72477cde27
Trizahwambua <wambuatrizzah5@gmail.com>
2023-09-20 15:17:06 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -2,37 +2,37 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include < stdlib.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include < signal.h>
-#include < dirent.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include < errno.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <signal.h>
 
 
 /*constants*/
-#define PATH_COMMAND 3
 #define EXTERNAL_COMMAND 1
 #define INTERNAL_COMMAND 2
+#define PATH_COMMAND 3
 #define INVALID_COMMAND -1
 
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
 /**
- * struct map - is a struct that maps command name to a function
- * @command_name: name of the command
- * @function: the function that executes the command
+ *struct map - a struct that maps a command name to a function 
+ *
+ *@command_name: name of the command
+ *@func: the function that executes the command
  */
 
 typedef struct map
 {
 	char *command_name;
-	void (*function)(char **command);
-}
-function_map;
+	void (*func)(char **command);
+} function_map;
 
 extern char **environ;
 extern char *line;
@@ -45,21 +45,21 @@ void print(char *, int);
 char **tokenizer(char *, char *);
 void remove_newline(char *);
 int _strlen(char *);
-void strchr(char *, char *);
+void _strcpy(char *, char *);
 
-/* helpers */
+/*helpers2*/
+int _strcmp(char *, char *);
+char *_strcat(char *, char *);
+int _strspn(char *, char *);
+int _strcspn(char *, char *);
+char *_strchr(char *, char);
+
+/*helpers3*/
 char *_strtok_r(char *, char *, char **);
 int _atoi(char *);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void ctrl_c_handler(int);
 void remove_comment(char *);
-
-/*helpers*/
-int strcmp(char *, char *);
-char *_strcat(char *, char *);
-int strspn(char *, char *);
-char *_strchr(char *, char *);
-char *strcspn(char *, char *);
 
 /*utils*/
 int parse_command(char *);
@@ -76,4 +76,4 @@ void quit(char **);
 extern void non_interactive(void);
 extern void initializer(char **current_command, int type_command);
 
-#endif SHELL_H
+#endif /*SHELL_H*/
